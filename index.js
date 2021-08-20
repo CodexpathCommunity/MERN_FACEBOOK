94fb7101ad97aa33fe295111fcec5d54f2f7a0f1
CodexpathCommunity <codexpath3@gmail.com>
2021-08-20 12:59:04 +0100

    connected mongodb

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const config = require("./server/config");
 
 dotenv.config(); // load .env file
-
-const CONNECTION_STRING = `mongodb://${config.db.url}/${config.db.name}`;
-
-mongoose.connect(CONNECTION_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-mongoose.connection.on("connected", () => {
-  console.log("Mongo has connected succesfully");
-});
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
 
 app.listen(8800, () => {
   console.log("Server is running on port 8800");
--- server/config/index.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  db: {
-    url: "localhost:21017",
-    name: "mernFB",
-  },
-};
-module.exports = config;
