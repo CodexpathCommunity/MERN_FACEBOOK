111432f8faceda169d07ea225a9b63024d612559
CodexpathCommunity <codexpath3@gmail.com>
2021-08-20 17:01:31 +0100

    working on auth connection

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,26 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-//register
-
-router.get("/register", async (req, res) => {
+//REGISTER
+router.post("/register", async (req, res) => {
   try {
-    const user = await new User({
-      username: "req.body.username",
-      email: "req.body.email",
-      password: "req.body.password",
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
     });
-    await user.save();
-    return res.send("user created");
+
+    //save user and respond
+    const user = await newUser.save();
+    res.status(200).json(user);
   } catch (err) {
-    return res.status(400).send(err);
+    res.status(500).json(err);
   }
 });
 
