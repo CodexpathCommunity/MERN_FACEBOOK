cfd1636fda29dece415d0ecfec8c90096cc936f0
CodexpathCommunity <codexpath3@gmail.com>
2021-08-20 16:42:41 +0100

    added the bcrypt package for hashing password

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("Hello World! authentication");
+//register
+
+router.get("/register", async (req, res) => {
+  try {
+    const user = await new User({
+      username: "req.body.username",
+      email: "req.body.email",
+      password: "req.body.password",
+    });
+    await user.save();
+    return res.send("user created");
+  } catch (err) {
+    return res.status(400).send(err);
+  }
 });
 
 module.exports = router;
